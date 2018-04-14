e1fb1b60b654be54f88d1cee54519cd4c914372a
Chris Lloyd <chris.lloyd@skyscanner.net>
2018-04-14 18:41:35 +0100

    Add loop

--- temperature.py
+++ temperature.py
@@ -13,7 +13,7 @@ except ImportError:
 import unicornhathd
 
 
-temperature = "28"
+temperature = 1
 
 # Use `fc-list` to show a list of installed fonts on your system,
 # or `ls /usr/share/fonts/` and explore.
@@ -31,31 +31,29 @@ unicornhathd.brightness(1.0)
 
 width, height = unicornhathd.get_shape()
 
-print("Starting...")
-print("Width: " + str(width))
-print("Height: " + str(height))
-
 font_file, font_size = FONT
 
 font = ImageFont.truetype(font_file, font_size)
 
 text_width, text_height = width, 7
 
-image = Image.new("RGB", (width, height), (0,0,0))
-draw = ImageDraw.Draw(image)
+while True:
+    image = Image.new("RGB", (width, height), (0,0,0))
+    draw = ImageDraw.Draw(image)
 
-offset_left = 0
+    offset_left = 0
 
-draw.text((0, 0), temperature, (0,255,0,255), font=font)
+    draw.text((0, 0), str(temperature), (0,255,0,255), font=font)
 
-for x in range(width):
-    for y in range(height):
-        pixel = image.getpixel((x, y))
-        r, g, b = [int(n) for n in pixel]
+    for x in range(width):
+        for y in range(height):
+            pixel = image.getpixel((x, y))
+            r, g, b = [int(n) for n in pixel]
 
-        unicornhathd.set_pixel(width - x - 1, y, r, g, b)
+            unicornhathd.set_pixel(width - x - 1, y, r, g, b)
 
-unicornhathd.show()
-time.sleep(100)
+    unicornhathd.show()
+    time.sleep(100)
+    temperature + 1
 
 unicornhathd.off()
