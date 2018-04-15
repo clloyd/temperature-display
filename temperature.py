dd6f48744069aee54e90c57c4f0925a2c07b7c16
Chris Lloyd <chris.lloyd@skyscanner.net>
2018-04-15 16:05:18 +0100

    TesT

--- temperature.py
+++ temperature.py
@@ -70,7 +70,7 @@ def pickColour(temp):
 
 def animateChange(iteration, x, y, r, g, b):
     if iteration < 8:
-        if x < 8 and x - 8 >= iteration:
+        if x < 8 and x >= 8 - iteration:
             unicornhathd.set_pixel(x, y, r, g, b)
         if x > 8 and x - 8 >= 8 - iteration:
             unicornhathd.set_pixel(x, y, r, g, b)
